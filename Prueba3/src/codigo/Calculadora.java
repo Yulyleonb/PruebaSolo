71f6176fa1f4b3783cfa639c398d97a718351a9f
yuliana <yuliana.leonb@ug.edu.ec>
2019-06-23 12:09:03 -0500

    dsdss

---
+++ Prueba3/src/codigo/Calculadora.java
@@ -0,0 +1,12 @@
+package codigo;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a=10;
+        int b=50;
+       System.out.println("Resulado"+ (a+b));
+	}
+
+}
